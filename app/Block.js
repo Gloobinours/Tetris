c604e308e600915b44716e179c6b014570299203
jhailos <jasonhailos2150@gmail.com>
2023-05-26 00:27:57 -0400

    erik parea bravo

--- app/Block.js
+++ app/Block.js
@@ -6,6 +6,37 @@ export class Block {
     this.y = 0;
   }
 
+  #isColliding() {
+    // check if block is colliding with another block or out of bounds
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = 0; j < this.tiles[i].length; j++) {
+        if (this.tiles[i][j] === 1) {
+          if (gameBoard[this.y + i][this.x + j] !== null) return true;
+        }
+      }
+    }
+  }
+
+  #rotateCollision(rotatedTiles) {
+    // while rotated block is colliding with another block, move it up
+    for (let z = 0; true; z++) {
+      for (let i = 0; i < rotatedTiles.length; i++) {
+        for (let j = 0; j < rotatedTiles[i].length; j++) {
+          if (rotatedTiles[i][j] === 1) {
+            if (gameBoard[this.y + i][this.x + j] !== null) {
+              this.y--;
+            } else return;
+            if (z > 2) {
+              this.y += 3;
+              return;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
   rotate() {
     const rotatedTiles = [];
     for (let i = 0; i < this.tiles[0].length; i++) {
@@ -15,26 +46,48 @@ export class Block {
       }
       rotatedTiles.push(newRow);
     }
-    this.tiles = rotatedTiles;
+
+    this.#rotateCollision(rotatedTiles)
 
     // if rotated block is out of bounds, move it back in bounds
-    if (this.x + this.tiles[0].length >= window.WIDTH)
-      this.x = window.WIDTH - this.tiles[0].length;
-    
-    if (this.y + this.tiles.length >= window.HEIGHT)
-      this.y = window.HEIGHT - this.tiles.length;
+    if (this.x + rotatedTiles[0].length >= window.WIDTH) {
+      this.x = window.WIDTH - rotatedTiles[0].length;
+    }
+    if (this.y + rotatedTiles.length >= window.HEIGHT) {
+      this.y = window.HEIGHT - rotatedTiles.length;
+    }
+
+    // set rotated tiles to current block
+    this.tiles = rotatedTiles;
   }
 
   moveLeft() {
-    if (this.x <= 0) return;
+    if (this.x <= 0) return; // block out of bounds
+    // check if block would collide with another block
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = 0; j < this.tiles[i].length; j++) {
+        if (this.tiles[i][j] === 1) {
+          if (gameBoard[this.y + i][this.x + j - 1] !== null) return;
+        }
+      }
+    }
+
     this.x--;
   }
   moveRight() {
-    if (this.x + this.tiles[0].length >= window.WIDTH) return;
+    if (this.x + this.tiles[0].length >= window.WIDTH) return; // block out of bounds
+    // check if block would collide with another block
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = 0; j < this.tiles[i].length; j++) {
+        if (this.tiles[i][j] === 1) {
+          if (gameBoard[this.y + i][this.x + j + 1] !== null) return;
+        }
+      }
+    }
     this.x++;
   }
   moveDown() {
-    if (this.isAtBottom()) return;
+    if (this.isAtBottom()) return; // block out of bounds
     this.y++;
   }
   snapDown() {
--- app/GameBoard.js
+++ app/GameBoard.js
@@ -11,16 +11,17 @@ let canvas = document.getElementById('gameBoard');
 let ctx = canvas.getContext('2d');
 let level = 1;
 let speed = 500/level;
+let score = 0;
 
 // list of blocks
 let blocks = {
-  "T": new Block([[0,1,0], [1,1,1]], 'purple', 3),
-  "L-left": new Block([[1,0,0], [1,1,1]], 'blue', 3),
-  "L-right": new Block([[0,0,1], [1,1,1]], 'orange', 3),
-  "Z-left": new Block([[1,1,0], [0,1,1]], 'red', 3),
-  "Z-right": new Block([[0,1,1], [1,1,0]], 'green', 3),
-  "bar": new Block([[1,1,1,1], []], 'cyan', 3),
-  "square": new Block([[1,1], [1,1]], 'yellow', 4)
+  "T": [[[0,1,0], [1,1,1]], 'purple', 3],
+  "L-left": [[[1,0,0], [1,1,1]], 'blue', 3],
+  "L-right": [[[0,0,1], [1,1,1]], 'orange', 3],
+  "Z-left": [[[1,1,0], [0,1,1]], 'red', 3],
+  "Z-right": [[[0,1,1], [1,1,0]], 'green', 3],
+  "bar": [[[1,1,1,1]], 'cyan', 3],
+  "square": [[[1,1], [1,1]], 'yellow', 4]
 }
 
 // create a game board
@@ -37,7 +38,39 @@ function createGameBoard() {
 function createRandomBlock() { // creates random block
   let blockNames = Object.keys(blocks);
   let randomBlockName = blockNames[Math.floor(Math.random() * blockNames.length)];
-  return blocks[randomBlockName];
+  return new Block(blocks[randomBlockName][0], blocks[randomBlockName][1], blocks[randomBlockName][2]);
+}
+
+// push a block to the gameBoard
+function placeBlock(block) {
+  for (let i = 0; i < block.tiles.length; i++) {
+    for (let j = 0; j < block.tiles[i].length; j++) {
+      if (block.tiles[i][j] === 1) {
+        window.gameBoard[block.y + i][block.x + j] = block.color;
+      }
+    }
+  }
+}
+
+// check if a row is full
+function isRowFull(row) {
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] === null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// clear a row and move everything down
+function clearRow(row) {
+  for (let i = row; i > 0; i--) {
+    window.gameBoard[i] = window.gameBoard[i-1];
+  }
+  window.gameBoard[0] = [];
+  for (let i = 0; i < window.WIDTH; i++) {
+    window.gameBoard[0][i] = null;
+  }
 }
 
 // Draw current block
@@ -70,7 +103,7 @@ function drawBlock(block) {
 function drawGameBoard() {
   ctx.translate(0.5, 0.5);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'cyan';
+  ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.strokeStyle = 'black';
   ctx.strokeRect(0, 0, canvas.width, canvas.height);
@@ -95,7 +128,6 @@ function drawGameBoard() {
     }
   }
   ctx.translate(-0.5, -0.5);
-  console.log(currentBlock.x, currentBlock.y);
 }
 
 function init() {
@@ -110,15 +142,31 @@ function init() {
 
   // initialize current block
   currentBlock = createRandomBlock();
-
-  // begin auto drop (gravity)
-  let autoDrop = setInterval(() => {
-    currentBlock.moveDown();
-  }, speed);
-  
 }
 
 init();
+
+// begin auto drop (gravity)
+setInterval(() => {
+  // begin auto drop (gravity)
+  currentBlock.moveDown();
+  if (currentBlock.isAtBottom()) {
+    setTimeout(() => {
+      if (currentBlock.isAtBottom()) {
+        placeBlock(currentBlock);
+        currentBlock = queue.dequeue();
+        queue.enqueue(createRandomBlock())
+        
+        // check for full rows
+        for (let i = 0; i < window.HEIGHT; i++) {
+          if (isRowFull(window.gameBoard[i])) {
+            clearRow(i);
+          }
+        }
+      }
+    }, speed);
+  }
+}, speed);
 
 // Key stroke listenener
 document.addEventListener('keydown', function(event) {
